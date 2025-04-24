119ec49460ab6b9b23598447be03e8f52d4d95b9
pranavdhumal9987 <pranavdhumal9987@gmail.com>
2025-04-24 11:17:57 +0530

    coderefactor

---
+++ src/main/java/com/example/makemytrip/Flights.java
@@ -0,0 +1,10 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Flights {
+    @GetMapping("/Flights")
+    public String getData() {return  "Please book your flight from Indigo kindly book ticket for New Delhi to anywhere at 10% discount" ; }
+}
---
+++ src/main/java/com/example/makemytrip/bus.java
@@ -0,0 +1,10 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class bus {
+    @GetMapping("/bus")
+    public String getData() {return  "Please book your bus tickets  from mmt kindly book ticket for New Delhi to anywhere at 10% discount" ; }
+}
